19aff6588155be27d3cf0fc40ae864e26663afb9
Daniel Malone <danieljmalone@gmail.com>
2017-05-10 22:46:15 -0500

    new regex

--- src/com/finepointstudios/Main.java
+++ src/com/finepointstudios/Main.java
@@ -6,9 +6,11 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-        String text = "Hi! How are you doing today?";
-        Pattern p = Pattern.compile("[A-z]+");
+        String text = "Hi! How are you doing today? Are you going to the zoo today? I am not!";
+        Pattern p = Pattern.compile("[A-Za-z]+");
         Matcher m = p.matcher(text);
+
+        // data above
         while (m.find()) {
             System.out.println("Found: " + m.group());
         }
